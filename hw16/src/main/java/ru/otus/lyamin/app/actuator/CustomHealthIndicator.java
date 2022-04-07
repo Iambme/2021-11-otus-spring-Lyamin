e91bc2d97afde2bbc406b8654b22ae3d3e9a3e4b
iambme <iambme@mail.ru>
2022-04-07 10:27:45 +0300

    правки по замечаниям

--- hw16/src/main/java/ru/otus/lyamin/app/actuator/CustomHealthIndicator.java
+++ hw16/src/main/java/ru/otus/lyamin/app/actuator/CustomHealthIndicator.java
@@ -1,23 +1,21 @@
 package ru.otus.lyamin.app.actuator;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.stereotype.Component;
-import ru.otus.lyamin.app.entity.Book;
-import ru.otus.lyamin.app.service.interf.BookService;
 
-import java.util.List;
+import java.util.Random;
 
 @Component
-@RequiredArgsConstructor
 public class CustomHealthIndicator implements HealthIndicator {
-    private final BookService bookService;
 
     @Override
     public Health health() {
-        List<Book> books = bookService.findAll();
-        return books.size() > 0 ? Health.up().withDetail("There is many books in library", books).build() :
-                Health.down().withDetail("message", "There is no books in library, something wrong").build();
+        return veryImportantCheck() ? Health.up().withDetail("message", "Look like everything is fine").build() :
+                Health.down().withDetail("message", "Something wrong").build();
+    }
+
+    private boolean veryImportantCheck() {
+        return new Random().nextBoolean();
     }
 }
