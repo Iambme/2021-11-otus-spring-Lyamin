25b299ca3760ed7efa316618a07fda728e06e22d
pavel.liamin <iambme@mail.ru>
2022-05-11 14:01:24 +0300

    подключен resilience4j

---
+++ hw18/src/main/java/ru/otus/lyamin/app/service/impl/AuthorServiceImpl.java
@@ -0,0 +1,59 @@
+package ru.otus.lyamin.app.service.impl;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.otus.lyamin.app.dao.AuthorRepository;
+import ru.otus.lyamin.app.entity.Author;
+import ru.otus.lyamin.app.exception.LibraryException;
+import ru.otus.lyamin.app.service.interf.AuthorService;
+
+import java.util.List;
+
+import static ru.otus.lyamin.app.util.ProblemsGenerator.generateProblem;
+
+@Service
+@AllArgsConstructor
+public class AuthorServiceImpl implements AuthorService {
+    private AuthorRepository authorRepository;
+
+
+    @Override
+    @Transactional(readOnly = true)
+    public Author findById(Long id) {
+        return authorRepository.findById(id).orElseThrow(() -> new LibraryException("Author not found with id " + id));
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Author findByName(String name) {
+        return authorRepository.findAuthorByName(name).orElseThrow(() -> new LibraryException("Author not found with name " + name));
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Author> findAll() {
+        generateProblem("service.AuthorServiceImpl.findAll");
+        return authorRepository.findAll();
+    }
+
+    @Override
+    @Transactional
+    public Author save(Author author) {
+        return authorRepository.save(validateAuthor(author));
+    }
+
+
+    @Override
+    @Transactional
+    public void deleteById(Long id) {
+        authorRepository.deleteById(id);
+    }
+
+    private Author validateAuthor(Author author) {
+        if (author.getName().isEmpty()) {
+            throw new LibraryException("Author name must be not empty");
+        }
+        return author;
+    }
+}
